9a4c76c7763586b75c288605373f90d539ab1451
unknown <lp411301@gmail.com>
2024-07-22 16:57:31 -0300

    listar dados da api

---
+++ src/pages/Inicial.js
@@ -0,0 +1,14 @@
+function Inicial() {
+    return(
+        <div>
+            <h1>Abas Bank</h1>
+            <div>
+                <button>Nova Conta</button>
+                <p>Não existe conta cadastrada</p>
+            </div>
+            Contas cadastradas
+        </div>
+    );
+}
+
+export default Inicial;
---
+++ src/pages/Listagem.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const Listagem = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://brasilapi.com.br/api/banks/v1');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once after initial render
+
+  return (
+    <div>
+        <h1>abas bank</h1>
+        <input></input>
+        <button>Consultar</button>
+        <ul>
+            {data.map(item => (
+            <li key={item.id}> 
+                <p>{'Name: '+ item.name} <br></br>
+                {'Code: '+ item.code} <br></br>
+                {'Ispb: ' + item.ipsb} <br></br>
+                {'Full Name: '+ item.fullName}</p>
+            </li>
+            ))}
+        </ul>
+    </div>
+  );
+};
+
+export default Listagem;
